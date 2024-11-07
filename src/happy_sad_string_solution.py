9f27adf31f13dedc03527f328cd50fda9fcb4696
Nb-coder-894 <nb0183@pleasantonusd.net>
2024-11-07 05:34:10 +0000

    first commit to understand dictionary

---
+++ src/happy_sad_string_solution.py
@@ -0,0 +1,26 @@
+"""
+input = "amaneatsindianorangeunderthesea"
+expected output "Hapyy String: amaneatsindianorangeunderthesea"
+
+input = "Jery Speaks Urdu"
+Expected Output "Sad String: Jery Speaks Urdu"
+
+input = "baaaii"
+
+"""
+
+"""
+Write your thought process here:
+
+
+"""
+
+# input_string = "amaneatsindianorangesunderthesea"
+# vowels = "aeiou"
+
+# for i in input_string:
+#     if i.lower() in vowels:
+#         found_vowels.add(i.lower())
+# if set(vowels) 
+# 
+# home work : understand what is a dictionary                
