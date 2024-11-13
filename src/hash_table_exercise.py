d3323d811f4d82c402885a3c84f5d2979d766f66
Nb-coder-894 <nb0183@pleasantonusd.net>
2024-11-13 05:42:03 +0000

    adventure with dict

---
+++ src/hash_table_exercise.py
@@ -0,0 +1,19 @@
+"""
+You are given a dictionary
+
+which maps as 1 -> red, 2 -> blue 3 -> yellow
+
+Write a code that will get value for 2
+
+Also try to get a value for 4
+        
+
+"""
+
+my_dict = {
+    1 : "red",
+    2 : "blue",
+    3 : "÷yellow"
+}
+
+print(my_dict[4])
