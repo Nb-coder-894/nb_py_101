f0f793fb2c9a774e0a545ed946fffa95f5cb9a04
Nb-coder-894 <nb0183@pleasantonusd.net>
2024-12-10 19:46:21 +0000

    My compound interest code

--- game.py/my_g.py
+++ game.py/my_g.py
@@ -7,11 +7,17 @@ import time
 # if number is correct, say correct and end
 # if the number incorrect, give more try. If 5 tryies done then say lose. 
 answer = random.randint(1,10)
+tries = 5
 print ("Guessing game.")
 time.sleep(1)
 print ("What is the number")
 if input == answer:
     print("correct. You win!! Congratulations!")
+elif input != answer:
+    tries =-1
+    print("Incorrect.")
+if tries == 0:
+    print(end)        
 
 
 
