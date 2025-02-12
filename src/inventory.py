ba107d42df96dbf6e2ce80c82660f2c68dc6f786
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-02-12 05:18:53 +0000

    additional changes

--- src/inventory.py
+++ src/inventory.py
@@ -1,11 +1,6 @@
-amazon_dict = {
-    "chairs" : "300" ,
-    "tables" : "200" ,
-    "toys" : "999" ,
-    "videogames" : "999999999999"
-}
-print (amazon_dict)
-print ("would you like to add something type y or n")
-if input == "y":
+import inventorycontents
+print (inventorycontents.amazon_dict)
+yOrN = input("would you like to add something type y or n? ")
+if yOrN == "y":
     print ("first please give the name of the item")
                                                                                                                  
