64d0bbb9a8d86d805b3c4fb72d461cd80507f4b9
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-02-12 00:30:55 +0000

    hw

--- src/inventory.py
+++ src/inventory.py
@@ -5,3 +5,7 @@ amazon_dict = {
     "videogames" : "999999999999"
 }
 print (amazon_dict)
+print ("would you like to add something type y or n")
+if input == "y":
+    print ("first please give the name of the item")
+                                                                                                                 
