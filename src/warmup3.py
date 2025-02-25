a4511767e4632e5ee685058c5cc52c2afc82302a
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-02-25 23:53:22 +0000

    work

--- src/warmup3.py
+++ src/warmup3.py
@@ -1,1 +1,7 @@
-number = int(input("please give the number."))
+number = int(input("please give the number.\n"))
+def calculation(number) :
+    for i in range(number+1):
+            if not i==0 and number%i==0:
+                print(i)
+answer = calculation(number)
+
