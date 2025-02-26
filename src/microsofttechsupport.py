5929173b486799c87ba42edfba837600b6da5664
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-02-26 22:55:45 +0000

    work

---
+++ src/microsofttechsupport.py
@@ -0,0 +1,6 @@
+import time
+print("Hello this is Microsoft Tech support. \n Please give your credit card info please")
+time.sleep(0.5)
+print("Your account appears to have been hacked. \n Because of this you will need to give us your information, and you need to buy some tokens from the nearby Target.")
+time.sleep(0.5)
+print("Please press 0 to hang up, 1 to contact our tech help, and 2 to get a refund.")
