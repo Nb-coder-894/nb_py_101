5741bb31e2dafae6893bd7b5aa9eee500017f32d
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-03-05 00:08:33 +0000

    work

--- src/numpywarmup.py
+++ src/numpywarmup.py
@@ -1,6 +1,6 @@
 import numpy as np
 a1D = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
-request = input(int("pls give the number"))
-if request == int:
-
-     print(request)
+request = int(input("give a number \n"))
+a1D *= request
+print(a1D)
+    
