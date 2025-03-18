99e0cf07b8ed48010ed5df4a6ed840a6d3677d15
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-03-18 23:29:43 +0000

    work

--- src/newworktodayhtmlandflask.py
+++ src/newworktodayhtmlandflask.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World"
+    from flask import render_template
+    
+    @app.route("/template")
+    def template_example():
+        return render_template("index.html", message="Hello from Flask!")
