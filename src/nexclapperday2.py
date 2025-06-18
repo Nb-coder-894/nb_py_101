66bd93de56a1a28105b8c353c758d4f72b1eae4b
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-06-18 16:37:12 +0000

    workstufffff

---
+++ src/nexclapperday2.py
@@ -0,0 +1,83 @@
+"""
+Learning about substrings and Taking characters from a string.
+
+substrings are parts of a string that can be assigned to a variable.
+
+For example:
+
+word = "abcdefgh"
+
+x = word[0:3]
+
+print(x)
+
+# This will create an output of "abc"
+
+y = word[0]
+
+# This will create an output of "a".
+
+#2: How to choose random items from a list. 
+#To choose random items from a list, first import randint.
+
+from random import randint
+
+items = ["apple", "banana", "orange"]
+
+randomVal = randint (0,2)
+
+x = items[randomVal]
+
+print(x)
+
+#This will create a random series of "apple", "banana", etc.
+
+#3: Choosing random characters from a string.
+
+#This is pretty similar to the last thing, but with a single string. I would think that this would combine the effects of substrings and randint.
+
+from random import randint
+
+word = "Three"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
