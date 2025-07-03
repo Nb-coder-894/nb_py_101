460fe4dbe1c86686013eb133436b345f316800fb
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-07-03 18:33:06 +0000

    day4p4

--- src/day_4/day_4_e2.py
+++ src/day_4/day_4_e2.py
@@ -1,1 +1,6 @@
 # This is exercise 2, where I have to make a function called square(n) that returns the square of a number, such as square(5) = 25.
+def square(n):
+    return n * n
+n = int(input("Please give a number.\n"))
+print("Your result is...\n" + str(square(n)))
+# This works.
