9f481ad2da6daa784b6e2b43a206f580a3a8a19f
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-07-07 04:07:40 +0000

    newsd

--- src/cryptography/encypt.py
+++ src/cryptography/encypt.py
@@ -4,6 +4,7 @@ key = b'4CGnk5hCgqlauPJxnbxgrbb46AobL-eJSq6-bwVw1Ic='
 cipher = Fernet(key)
 
 encrypted_info = b"""
+
 """
 
 encrypted = cipher.encrypt(encrypted_info)
--- src/cryptography/hiddenbookreview.py
+++ src/cryptography/hiddenbookreview.py
@@ -8,8 +8,8 @@ cipher = Fernet(key)
 encrypted_report = b"gAAAAABoa0ZDpfE6ggLMDW19gGNWFG7OgXvJXGpNpO1-3ieE6ScRgNw20u8VhgdrVAhbMDhaFkVRPI-nbefeLuyV46rzEFbbZxrDH7wATPjc0yZEJvhft4Js24ZYLz1jB5lHm99b2V63r-kd8j_mF5_Nqba9vZojfi1IOq9mZc6-VDukkGhB3q1TslBxheW5MPB5e3j4a8tW'"  # encrypted bytes
 
 def main():
-    password = int(input("Enter password: "))
-    if password == 2013:
+    password = float(input("Enter password: "))
+    if password == 2013.2012201120102009200820072005:
         # Decrypt and print
         decrypted = cipher.decrypt(encrypted_report).decode()
         print(decrypted)
