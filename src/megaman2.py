607bb0f9c2fb61ce0bfa373ade79d9d050f80843
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-07-08 03:49:34 +0000

    astasdgf435

--- src/megaman2.py
+++ src/megaman2.py
@@ -1,30 +1,19 @@
 import time
 print("Now running Megaman2 RPG edition\n Version 1.0.0")
 time.sleep(1)
-print("You are megaman.\n")
+print("You are megaman.\n You were created to stop the evil forces of Dr.Wily.\n However, after you were created, Dr.Wily created 8 Robo Masters to stop you. \n You must defeat the evil Dr.Wily and stop all 8 of his robots from wreaking havoc \n upon the world...")
+player_data = {
+    "health" : 100,
+    "weapons" : ["Mega Buster"],
+    "stage_selected" : None,
+    "Wily_Tower_Mode" : False
+}
 time.sleep(1)
 print("Now entering stage select.\n")
-weapon_inventory = []
-x_position = 0
-y_position = 0
-while True:
-    weapon_inventory.append("Mega_Buster")
-    stage_select = input("Which stage? Air Man, Crash Man, Heat Man, Bubble Man, Wood Man, Quick Man, Flash Man, or Metal Man?")
-    stage_select = stage_select.lower()
-    if stage_select == "metal man":
-        print("Now loading Metal Man stage...\n Get your weapons ready...")
-        time.sleep(2)
-        print("Now in the starting area of Metal Man. \nTo select your weapon, type in w. \n To move, type in m. \n To jump, press j. ")
-        for i in range (1,300):
-            move_request = input("What will you do? Move using m? Equip weapon using w? \n Jump using J?")
-            move_request = move_request.lower()
-            if move_request == "w":
-                print(f"Your selected weapon is {weapon_inventory}")
-            elif move_request == "m":
-                move_direction = input("Forward or backward? type in f or b to do that.")
-                move_direction = move_direction.lower()
-                if move_direction == "b" and x_position == 0:
-                    print("You can't do that")
-                elif move_direction == "f"    
+player_data("stage_selected") == str(input("Which Robo Master will you defeat? \n Air Man, Crash Man, Bubble Man, Metal Man, \n Quick Man, Wood Man, Heat Man, or Flash Man? \n Oh, and please note:\n You cannot just instantly go and fight each Robo Master. \n They are in different parts of the world, and are in different areas.\n You must traverse each Robo Masters's respective domain...\n and fight your way to the boss, and then defeat the boss.\n Each boss has a secret weakness to a certain ability, but you don't know what those weaknesses are.\n Also, some of the minions of those Robo Masters also have weakness to certain abilities...\n but you don't know them (yet!)\n\n Anyway, which stage do you choose? "))
+player_data("stage_selected") == player_data("stage_selected").lower()
+if player_data("stage_selected") == "Flash Man":
+    print("Now loading Flash Man stage...\n Get your weapons ready")
+    print(player_data)
 
    
