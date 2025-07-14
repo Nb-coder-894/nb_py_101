58a60065aad8a55470d212ef035379e08c3dc628
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-07-14 21:58:37 +0000

    tester123

--- src/megaman2/megaman2.py
+++ src/megaman2/megaman2.py
@@ -5,13 +5,17 @@ print("You are Megaman. \n You were created by Dr.Light to stop the evil intenti
 time.sleep(3.4)
 player_data = {"Health" : 100,
                "Weapons" : ["Mega Buster"],
-               "Stages_Cleared" : []                           
+               "Stages_Cleared" : [],
+               "Wily_Tower_Mode" : False,
+               "Normal_Mode" : None,
+               "Hard_Mode" : None,
+               "Impossible_Mode" : None                           
                }
-weapon_inventory = player_data["Weapons"]
+weapon_inventory = player_data["Weapons"]    
 print("Now entering stage select")
 time.sleep(0.001)
 stage_select = input("Which stage will you chose?\n Your options are Bubble Man, Metal Man, Air Man, Crash Man,\n Flash Man, Heat Man, Quick Man, and Wood Man. \n Type in the name of the Robo Master you would like to fight. \n Oh, and please note: You cannot just go and fight them. They are in 8 different areas around the world, and you must navigate their stages in order to fight them. \n ")
 stage_select = stage_select.lower()
-if stage_select == "metal man":
-    print("Now loading Metal Man stage...\n")
+if stage_select == "flash man":
+    print("Now loading Flash Man stage...\n")
     time.sleep(4.1)
