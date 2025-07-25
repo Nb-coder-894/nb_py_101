0020111d805a053d96a6f4f94e2e0781849403b8
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-07-25 17:50:08 +0000

    exercises2.0

---
+++ src/chained_func.py
@@ -0,0 +1,36 @@
+"""
+task:
+ask for a number
+if number is even
+    -> it will print "start run"
+    -> it will call another function that will print from that number to 100 in an arithmetic progression with cd = the same number
+    -> then it will print summing 
+    --> it will call another function to do the sum
+
+if number is odd
+    -> it will print "oddball out"
+    -> it will do a Negative arithmetic progression starting from 100 till the number is reached
+    -> it will call another function( same one as in above) to sum
+
+"""
+
+number = int(input("Please give a number"))
+def main(odd_or_even):
+    if odd_or_even % 2 == 0:
+        print("Start Run")
+        def soemthing(arithmetic_Progression_maker_even):
+            n = (100-arithmetic_Progression_maker_even) / arithmetic_Progression_maker_even + 1
+            print(n)
+            n = int(n)
+            for i in range(1,n):
+                arithmetic_progression = [2]
+                arithmetic_progression.append(2+number)
+            print(arithmetic_progression)    
+        soemthing(number)
+           
+"""
+"""
+ 
+
+if __name__ == "__main__":
+    main(number)
