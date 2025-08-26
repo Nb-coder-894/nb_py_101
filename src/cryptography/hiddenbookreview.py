553c35416db4403921cb09cd1773c8f723c7d81e
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-08-26 18:45:25 +0000

    nowcommitingunresovledchanges

--- src/cryptography/hiddenbookreview.py
+++ src/cryptography/hiddenbookreview.py
@@ -5,11 +5,11 @@ key = b'4CGnk5hCgqlauPJxnbxgrbb46AobL-eJSq6-bwVw1Ic='  # Use Fernet.generate_key
 cipher = Fernet(key)
 
 # Encrypted book report (encrypt your text once)
-encrypted_report = b"gAAAAABoa0ZDpfE6ggLMDW19gGNWFG7OgXvJXGpNpO1-3ieE6ScRgNw20u8VhgdrVAhbMDhaFkVRPI-nbefeLuyV46rzEFbbZxrDH7wATPjc0yZEJvhft4Js24ZYLz1jB5lHm99b2V63r-kd8j_mF5_Nqba9vZojfi1IOq9mZc6-VDukkGhB3q1TslBxheW5MPB5e3j4a8tW'"  # encrypted bytes
+encrypted_report = b"gAAAAABoa0TaeklyRnE7tm90myCL1YMl5Ne3oCDkafcBkZy-n_vrePp7L3BETwxITqr5O6BUOrrSiZmLjqXrYX80EU8tnA0UCoFsvg2nJl4FaN3zhKbjwBBlCRH28NO2uEHYRxCX9NjWmjkjQurr6kmTG719dWR86AppU13KZVtEsqH2n4aPMqEnJY98deE-yLqGNbZUdlynb__x9SCCU9kGb4B2RnaUjyQrjNGZKIOxC7Helh1fanAJdEHmKtRa67QQ618n8deHsxRBtZQ841lw_fmZjjVIT48bXLv4KvoT790kDHGHiEYJ6dcqvoENhze2yLRrKy1LxiCVsZEe4sZDH-qcVLYIi8SP1NBVFpaVLbSvfxFcM_FxyPdUWUJlD-P7PrZ5a__OS6_S6dGzeehUIcyp8_DV6pjbDd5Ka85eRDiBvR2dOGc"  # encrypted bytes
 
 def main():
-    password = float(input("Enter password: "))
-    if password % 7 == 0 and password % 21 == 0 and password % 3 == 0:
+    password = (input("Enter password: ")).lower
+    if password == "sunetro":
         # Decrypt and print
         decrypted = cipher.decrypt(encrypted_report).decode()
         print(decrypted)
