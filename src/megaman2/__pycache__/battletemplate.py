46804b76d920c1bb78ee694e09a7db8fc9c322d5
Nb-coder-894 <nb0183@pleasantonusd.net>
2026-05-21 21:16:14 +0000

    newgame

--- src/megaman2/__pycache__/battletemplate.py
+++ src/megaman2/__pycache__/battletemplate.py
@@ -5,7 +5,7 @@ time.sleep(2)
 print("Please standby:")
 time.sleep(2)
 enemy_dict = ["coolguy", "ChuckBot"]
-print(f"You are now battling the {enemy_dict(0)}")
+print(f"You are now battling the enemy")
 while True:
     Enemy_HP = 300
     Player_HP = 500
@@ -14,9 +14,28 @@ while True:
     elif Enemy_HP > 0:
         print("The battle ensues...")
         Enemy_attack = random.randint(0,200)
-        print(f"The {enemy_dict(0)} damaged you with {Enemy_attack} damage!")
+        print(f"The enemy damaged you with \n {Enemy_attack} damage!")
         Player_HP = Player_HP - Enemy_HP
     if Player_HP <= 0:
-        print()             
+        print("YOU DIED")
+    elif Enemy_HP > 0 and Player_HP > 0:
+        player_command = input("what will you do? \n type i for info about what you can do.").lower()
+        if player_command == "i":
+            print("Attack with typing in a, heal slightly with h, \n defend with d, \n and run away with r. \n You will get hurt if you run away, though")
+            time.sleep(4)
+        elif player_command == "a":
+            player_attack = random.randint(0,100)
+            print(f"You attacked the enemy! You dealt {player_attack} damage.")
+            time.sleep(4)
+        elif player_command == "r":
+            Player_HP = random.randint (0,500) / 2
+            print("You shouldn't have run away.\n")
+            time.sleep(4)
+            print("Don't give up.")
+            break
+        elif player_command == "d":
+            Enemy_attack == (1/random.randint(1,4))  * Enemy_attack
+            print(f"You defended. Damaged reduced.") 
+            time.sleep(4)                          
 
 
