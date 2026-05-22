230f2dca0c01a9c5ddcce590c96d93ad434e0d1a
Nb-coder-894 <nb0183@pleasantonusd.net>
2026-05-22 16:56:12 +0000

    interesting.very

--- src/megaman2/__pycache__/battletemplate.py
+++ src/megaman2/__pycache__/battletemplate.py
@@ -9,16 +9,15 @@ print(f"You are now battling the enemy")
 while True:
     Enemy_HP = 300
     Player_HP = 500
-    if Enemy_HP <= 0:
+    if Enemy_HP <= 0: 
         print("You have won.")
-    elif Enemy_HP > 0:
+    elif Player_HP <= 0:
+        print("YOU DIED")
+    elif Enemy_HP > 0 and Player_HP > 0:
         print("The battle ensues...")
         Enemy_attack = random.randint(0,200)
         print(f"The enemy damaged you with \n {Enemy_attack} damage!")
         Player_HP = Player_HP - Enemy_HP
-    if Player_HP <= 0:
-        print("YOU DIED")
-    elif Enemy_HP > 0 and Player_HP > 0:
         player_command = input("what will you do? \n type i for info about what you can do.").lower()
         if player_command == "i":
             print("Attack with typing in a, heal slightly with h, \n defend with d, \n and run away with r. \n You will get hurt if you run away, though")
