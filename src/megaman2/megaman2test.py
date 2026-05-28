3939d2702938b7e0f74fe2e033a9eda9cfd41d37
Nb-coder-894 <nb0183@pleasantonusd.net>
2026-05-28 18:57:22 +0000

    interesting.very2

--- src/megaman2/megaman2test.py
+++ src/megaman2/megaman2test.py
@@ -142,7 +142,7 @@ def zone_check(psuedo_zone_checker):
                 move_select_directional_pos = input("Okay, forward or backward? You can use f or b to explain that.").lower()
                 if move_select_directional_pos == "f":
                     print("Okay. You choose to move forward.")
-                    psuedo_zone_checker[player_data["x_pos"]] += 1
+                    psuedo_zone_checker["x_pos"] += 1
                     print(f"TESTING \n Your x_pos is {psuedo_zone_checker["x_pos"]}")
                     
                 if move_select_directional_pos == "b":
@@ -152,9 +152,82 @@ def zone_check(psuedo_zone_checker):
                             continue
                         else: psuedo_zone_checker["x_pos"]-= 1
             elif move_select == "w":
-                print(f"You have" + psuedo_zone_checker["Weapons"])
+                print(f"You have" + {psuedo_zone_checker["Weapons"]})
             elif move_select == "j":
-                print("Okay, you choose to jump. You moved forward.")                
+                print("Okay, you choose to jump. You moved forward.")
+            if player_data["x_pos"] == 5:
+                player_data["Battle_Mode"] = True
+                print("!" * 100)
+                print("!" * 100)
+                print("!" * 100)
+                print("!" * 100)
+                print("An enemy approches. You are now fighting a ChuckBot.")
+                import time
+                import random
+                print("Now running battle_template version 1.0.0....")
+                time.sleep(2)
+                print("Please standby:")
+                time.sleep(2)
+                enemy_dict = ["coolguy", "ChuckBot"]
+                print(f"You are now battling the enemy")
+                while True:
+                    Enemy_HP = 300
+                    Player_HP = 500
+                    if Enemy_HP > 0 and Player_HP > 0:
+                        print("The battle ensues...")
+                        Enemy_attack = random.randint(100,200)
+                        print(f"The enemy damaged you with \n {Enemy_attack} damage!")
+                        player_command = input("what will you do? \n type i for info about what you can do.").lower()
+                        Player_HP -= Enemy_HP
+                    if player_command == "i":
+                        print("Attack with typing in a, heal slightly with h, \n defend with d, \n and run away with r. \n You will get hurt if you run away, though. ")
+                        time.sleep(4)
+                        continue
+                    elif player_command == "a":
+                        player_attack = random.randint(0,100)
+                        print(f"You attacked the enemy! You dealt {player_attack} damage.")
+                    elif player_command == "r":
+                        Player_HP /= 2 * random.randint (0,500)
+                        print("You shouldn't have run away.\n")
+                        time.sleep(4)
+                        print("Don't give up.")
+                        break
+                    elif player_command == "d":
+                        Enemy_attack *= (1/random.randint(1,4))  
+                        print(f"You defended. Damage reduced by {Enemy_attack}") 
+                        time.sleep(4)
+                    elif player_command == "h":
+                        Player_heal = random.randint(0,200)
+                        Player_HP += Player_heal
+                        print(f"Your health is now {Player_HP}[this is a test] \n Also you recovered {Player_heal} Health.")
+                    else:
+                        print("command not understood")
+                        continue 
+
+
+
+
+            #Note: This is the basic version that does nto have any redirecting.
+            # That is why the healthbar for the player is higher than normal
+            # In reality, a bunch of functions will be used to redirect the user.                                           
+
+
+
+                    while Flash_Man == True:
+                        if Enemy_HP <= 0:
+                            print("You win")
+                            break
+                        elif Player_HP <= 0:
+                            print("Ded")
+                            break
+        
+
+
+
+
+                               
+
+                    
     else:
         print("ERROR 400")
         time.sleep(1.2)
@@ -162,15 +235,15 @@ def zone_check(psuedo_zone_checker):
         print("Now redirecting into stage select...")
         check_stage_select(player_data)    
 zone_check(player_data)
-def location_checker(location_checker):
-    while True:
-        if location_checker["x_pos"] == 5:
-            location_checker["Battle_Mode"] = True
+while player_data["Flash_Man"] == True:
+        if player_data["x_pos"] == 5:
+            player_data["Battle_Mode"] = True
             print("!" * 100)
             print("!" * 100)
             print("!" * 100)
             print("!" * 100)
             print("An enemy approches. You are now fighting a ChuckBot.")
+            #
             import time
             import random
             print("Now running battle_template version 1.0.0....")
